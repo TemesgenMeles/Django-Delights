a2628994a4daf2af65da6909ceee3126e2df8aac
TemesgenMeles <temesgen12m1@gmail.com>
2024-03-14 19:28:27 +0300

    creating Ingradint model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+units = {
+    "tbsp": "big table spune",
+    "lbs": "table spune",
+    "gr": "gram",
+    "kg": "kilogram",
+    "ounce": "ounce",
+    "ml": "mililitre",
+    "li": "litre"
+}
 # Create your models here.
+class Ingradient(models.Model):
+    states = {
+        "li": "liquid",
+        "so": "solid"
+    }
+    Name = models.CharField(max_length=50)
+    State = models.CharField(max_length=2, choice=states)
+    Quantity = models.FloatField(default=0)
+    Unit = models.CharField(max_length=6, choice=units)
+    Unit_price = models.FloatField(default=0)
