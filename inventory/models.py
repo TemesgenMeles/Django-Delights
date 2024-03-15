79ff3c5ef4f3108a0cb9033b52ad39424abbd45b
TemesgenMeles <temesgen12m1@gmail.com>
2024-03-15 11:50:35 +0300

    finishing up the model

--- inventory/models.py
+++ inventory/models.py
@@ -19,3 +19,20 @@ class Ingradient(models.Model):
     Quantity = models.FloatField(default=0)
     Unit = models.CharField(max_length=6, choice=units)
     Unit_price = models.FloatField(default=0)
+
+class MenuItem(models.Model):
+    Title = models.CharField(max_length=30)
+    Price = models.FloatField()
+    Status = models.BooleanField(default=True)
+    
+class RecipeRequirement(models.Model):
+    Menu_item = models.ForeignKey("MenuItem", verbose_name=("Menu Item"), on_delete=models.CASCADE)
+    Ingradint = models.ForeignKey("Ingradient", verbose_name=("Recipe"), on_delete=models.CASCADE)
+    Quantity = models.FloatField(default=0, balnk=False, Null=False)
+    Unit = models.CharField(max_length=6, choice=units)
+    
+class Purchase(models.Model):
+    Menu_item = models.ForeignKey("MenuItem", verbose_name=("Menu Item"))
+    Timestamp = models.DateTimeField(auto_now=True, auto_now_add=True)
+    Quantity = models.IntegerField(default=1)
+    Total_price = models.FloatField()
