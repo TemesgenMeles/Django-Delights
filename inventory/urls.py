c992bc7080f6aa3fca1e1a9baeb988a722f00e4e
TemesgenMeles <temesgen12m1@gmail.com>
2024-03-18 22:59:19 +0300

    purchase history template

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("Recipes/", views.ShowRecipe, name="recipes"),
     path("Stocks/", views.ShowIngradients, name="stocks"),
     path("Shop/", views.Profit_revenue, name="purchase"),
-    path("Shop/All", views.Purchases, name="purchaseHistory"),
+    path("Shop/All", views.All_purchase, name="purchaseHistory"),
     path("Profit/", views.Profit_revenue, name="profit"),
     path("Logout/", views.Logout, name="logout"),
 ]
